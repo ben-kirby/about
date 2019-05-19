7d24218eca9d5ae995c872ccd48027ba1deaa0c2
Ben Kirby <benkrby@gmail.com>
2019-05-19 14:03:50 -0700

    imported the rest of the sections

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Header from './components/header';
+import About from './components/about';
+import Work from './components/work';
+import Background from './components/background';
+import Interests from './components/interests';
+import Contact from './components/contact';
 
 const styles = {
   appContainer: styled.div`
@@ -17,6 +22,11 @@ function App() {
   return (
     <styles.appContainer>
       <Header/>
+      <About/>
+      <Background/>
+      <Work/>
+      <Interests/>
+      <Contact/>
     </styles.appContainer>
   );
 }
