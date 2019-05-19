a407d326938b2354c98c13d655543cf337fefae6
Ben Kirby <benkrby@gmail.com>
2019-05-19 14:05:04 -0700

    added generic container and section header

---
+++ src/styles/styledComponents.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 60%;
+	align-items: center;
+`;
+
+export const SectionTitle = styled.h2`
+
+`;
