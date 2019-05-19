ed6ee8010345fc2bb44c555afa6d9fe1425a3d60
Ben Kirby <benkrby@gmail.com>
2019-05-19 14:05:15 -0700

    added constants for contact info

---
+++ src/constants/aboutInfo.js
@@ -0,0 +1,4 @@
+export const linkedInUrl = 'https://linkedin.com/in/ben-in-pdx/';
+export const gitHubUrl = 'https://github.com/ben-kirby/';
+export const about = 'about me';
+export const background = 'i do stuff';
