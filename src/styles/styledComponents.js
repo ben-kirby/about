52802c7e7056c492c0b9b122ed621849d9b49399
Ben Kirby <benkrby@gmail.com>
2019-05-20 10:37:58 -0700

    added style components

--- src/styles/styledComponents.js
+++ src/styles/styledComponents.js
@@ -3,10 +3,15 @@ import styled from 'styled-components';
 export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 60%;
+	width: 70%;
 	align-items: center;
+	background: rgba(255, 255, 255, 0.5);
+	padding: 0 5% 0 5%;
 `;
 
 export const SectionTitle = styled.h2`
+`;
 
+export const SectionContent = styled.div`
+	align-items: center;
 `;
