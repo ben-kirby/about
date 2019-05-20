e5a62108f2fcec91191d40b558559b0ac54dca58
Ben Kirby <benkrby@gmail.com>
2019-05-20 10:37:46 -0700

    added bg info

--- src/constants/aboutInfo.js
+++ src/constants/aboutInfo.js
@@ -1,4 +1,30 @@
 export const linkedInUrl = 'https://linkedin.com/in/ben-in-pdx/';
+
 export const gitHubUrl = 'https://github.com/ben-kirby/';
+
 export const about = 'After graduating from Portland State University in 2015 I began working in the tech industry for an iPad-based Point of Sale developer. While there, I was able to get some good exposure to the development world working alongside our QA team and by developing tools for our customer care team to speed up their workflows. I was really enjoying the development work that I was doing so I decided to take Epicodus\' Intro to Programming course in the evenings. By the time the class was finished, I was hooked. A few months later I left my job to enroll in the full-time program. When I\'m not coding, you can find me cooking for my friends, pestering my cat Miso while he tries to nap, and tracking down a good beer. Sometimes all at the same time.';
-export const background = 'about me';
+
+export const background = [
+	{
+		title: 'Operations Specialist',
+		company: 'ShopKeep',
+		description: 'things',
+		start: 'start',
+		end: 'end'
+	},
+	{
+		title: 'Senior Advisor',
+		company: 'ShopKeep',
+		description: 'things',
+		start: 'start',
+		end: 'end'
+	},
+	{
+		title: 'Customer Care Advisor',
+		company: 'ShopKeep',
+		description: 'things',
+		start: 'start',
+		end: 'end'
+	}
+];
+
