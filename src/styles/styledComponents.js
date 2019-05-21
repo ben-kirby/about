178075ba8d347093500589c6b49e8b9f61774576
Ben Kirby <benkrby@gmail.com>
2019-05-20 23:51:30 -0700

    added font and styling

--- src/styles/styledComponents.js
+++ src/styles/styledComponents.js
@@ -1,4 +1,11 @@
 import styled from 'styled-components';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+	google: {
+		families: ['Quicksand']
+	}
+});
 
 export const Container = styled.div`
 	display: flex;
@@ -6,10 +13,12 @@ export const Container = styled.div`
 	width: 80%;
 	align-items: center;
 	background: rgba(255, 255, 255, 0.5);
-	padding: 0 25px 0 25px;
+	padding: 0 25px 25px 25px;
 `;
 
 export const SectionTitle = styled.h2`
+	color: #247BA0;
+	font-family: Quicksand;
 `;
 
 export const SectionContent = styled.div`
