2c04a81077d788e3ca0ecf146f757d6ceacb512f
Ben Kirby <benkrby@gmail.com>
2019-05-20 23:51:20 -0700

    added descriptions to work and another job

--- src/constants/aboutInfo.js
+++ src/constants/aboutInfo.js
@@ -2,9 +2,16 @@ export const linkedInUrl = 'https://linkedin.com/in/ben-in-pdx/';
 
 export const gitHubUrl = 'https://github.com/ben-kirby/';
 
-export const about = 'After graduating from Portland State University in 2015 I began working in the tech industry for an iPad-based Point of Sale developer. While there, I was able to get some good exposure to the development world working alongside our QA team and by developing tools for our customer care team to speed up their workflows. I was really enjoying the development work that I was doing so I decided to take Epicodus\' Intro to Programming course in the evenings. By the time the class was finished, I was hooked. A few months later I left my job to enroll in the full-time program. When I\'m not coding, you can find me cooking for my friends, pestering my cat Miso while he tries to nap, and tracking down a good beer. Sometimes all at the same time.';
+export const about = 'After graduating from Portland State University in 2015 I began working in the tech industry for an iPad-based Point of Sale developer. While there, I was able to get some good exposure to the development world by working alongside our QA team and developing tools for our customer care team to speed up their workflows. I was really enjoying the work that I was doing so I decided to take Epicodus\' Intro to Programming course in the evenings. By the time the class was finished, I was hooked. A few months later I left my job to enroll in the full-time program. When I\'m not coding, you can find me cooking for my friends, pestering my cat Miso while he tries to nap, and tracking down a good beer. Sometimes all at the same time.';
 
 export const background = [
+	{
+		title: 'Intern',
+		company: 'DevelopmentNow',
+		description: 'things',
+		start: '4/2019',
+		end: '5/2019'
+	},
 	{
 		title: 'Operations Specialist',
 		company: 'ShopKeep',
@@ -64,26 +71,25 @@ export const work = [
 	{
 		title: 'Urban Farmer',
 		github: 'https://github.com/ben-kirby/Urban-Farmer',
-		description: 'urban farmer'
+		description: 'Urban Farmer was made while at an internship at DevelopmentNow. It lets users create an account, add inventory, make sales, and view their sales history.',
+		build: 'React Native, Firebase'
 	},
 	{
 		title: 'Messenger Hunt',
 		github: 'https://github.com/ben-kirby/messenger_hunt',
-		description: 'messenger hunt'
+		description: 'Messenger Hunt cherry-picks several concepts from a few different social media apps: Snapchat, FourSquare, and Twitter. While at a location, users can check-in and leave a short, message for another user. The twist is that the other user cant see the message until they go to the same location and check in as well.',
+		build: 'JavaScript, Ruby, Rails'
 	},
 	{
 		title: 'Quizzle Me Timbers',
 		github: 'https://github.com/ben-kirby/quizzle-me-timbers',
-		description: 'quizzle'
+		description: 'Quizzle Me Timbers is a trivia night hosting web-app that allows players to join teams with a game code, and lets hosts generate a list of questions and answers from an API call. When teams enter their answers, their answers go to the host for checking. It\'s also pirate themed.',
+		build: 'TypeScript, Angular, Firebase'
 	},
 	{
 		title: 'Lift',
 		github: 'https://github.com/ben-kirby/Lift',
-		description: 'lift'
+		description: 'Lift was an exercise in design and CSS. The site looks identical to Lyft\'s website as it looked in early 2019, except it features planes instead of cars. Thye\'ve since changed their look up a bit, but the site still looks good. It also gave me a chance to work in Adobe Illustrator to make the Logo.',
+		build: 'CSS, Angular, TypeScript, Adobe Illustrator'
 	},
-	{
-		title: 'Pollster',
-		github: 'https://github.com/ben-kirby/pollster',
-		description: 'pollster'
-	}
 ];
